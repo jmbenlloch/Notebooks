f3fcea71d9f08db9dcb4ff64ea8c42b9d81e74c8
Juan Gomez Cadenas <jjgomezcadenas@juans-macbook-pro-2.home>
2017-01-16 10:41:22 +0100

    Improvements to manage.sh and to diomira nb
    In manage.sh, name of the enviroment variable NBDIR has been changed
    to IC_NOTEBOOK_DIR for clarity. The diomira NB has been modified to
    propagate the new name, and a few improvements have been added

--- manage.sh
+++ manage.sh
@@ -10,20 +10,21 @@ function python_version_env {
     # Activate the relevant conda env
     source activate IC${PYTHON_VERSION}
     # Set NB environment variables and download database
-    nb_env
+    notebook_env
 }
 
 
-function nb_env {
+function notebook_env {
 	if [ -z $ICTDIR ]; then
 		echo "ERROR: $ICTDIR is not set, run your IC manage.sh first"
 	else
-    	export NBDIR=`pwd`
-	    echo NBDIR set to $NBDIR
+        export NBDIR=`pwd`
+    	export IC_NOTEBOOK_DIR=$NBDIR/invisible_cities
+	echo IC_NOTEBOOK_DIR set to $IC_NOTEBOOK_DIR
     
     	echo setting PYTHONPATH
-	    export PYTHONPATH=$ICTDIR
-	    echo PYTHONPATH is set to $PYTHONPATH
+	export PYTHONPATH=$ICTDIR
+	echo PYTHONPATH is set to $PYTHONPATH
 	fi
 }
 
@@ -33,13 +34,13 @@ function nb_env {
 
 case $COMMAND in
     python_version_env)      python_version_env ;;
-    nb_install)              nb_install ;;
+    notebook_env)            notebook_env ;;
     
     *) echo Unrecognized command: ${COMMAND}
        echo
        echo Usage:
        echo
        echo source $0 python_version_env X.Y
-       echo soruce $0 nb_env
+       echo source $0 notebook_env
        ;;
 esac
